2a7fe77a9299f03f597c40c2c62b198c08efc202
zhaotisheng <912980115@qq.com>
2018-05-08 19:10:21 +0800

    添加log日志集成

--- colove-web/src/main/java/xin/colove/web/ConsumerApplication.java
+++ colove-web/src/main/java/xin/colove/web/ConsumerApplication.java
@@ -2,19 +2,28 @@ package xin.colove.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import lombok.extern.slf4j.Slf4j;
 
 
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @SpringBootApplication
+@Slf4j
 public class ConsumerApplication {
 
-//    @Bean
-//    @LoadBalanced
-//    RestTemplate restTemplate ()  {
-//        return new  RestTemplate (); 
-//    }
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate ()  {
+        return new  RestTemplate (); 
+    }
     
 	public static void main(String[] args) {
+//	    log.debug("starting.....");log.info("starting.....");log.warn("starting.....");log.error("starting.....");
 		SpringApplication.run(ConsumerApplication.class, args);
+		
 	}
 }
--- colove-web/src/main/java/xin/colove/web/controller/ConsumerController.java
+++ colove-web/src/main/java/xin/colove/web/controller/ConsumerController.java
@@ -1,14 +1,31 @@
-/*
- * Copyright (C) 2012~2018 北京屏芯科技有限公司 保留所有权利
- */
 package xin.colove.web.controller;
 
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import lombok.extern.slf4j.Slf4j;
+import xin.colove.web.ConsumerApplication;
 
 /**
  * 
  * @author  Administrator
  * @version 1.0.0
  */
+@RestController
+@Slf4j
 public class ConsumerController {
-
+    
+    @Autowired
+    RestTemplate restTemplate;
+    
+    String url="http://HELLO-SERVICE-IDEA-DEMOIDEA";
+    
+    @RequestMapping(value ="/ribbon-consumer",  method =RequestMethod.GET)
+    public  String  helloConsumer  (String name){
+        return  restTemplate.getForEntity(url+"/hello?name={1}",String.class,name).getBody();
+    }
 }
